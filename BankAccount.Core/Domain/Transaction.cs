4cccbb5678c14469004ac99ac1238f4e9a437937
JRybelis <frizjr@protonmail.com>
2025-01-19 00:11:57 +0200

    Add transaction model

---
+++ BankAccount.Core/Domain/Transaction.cs
@@ -0,0 +1,19 @@
+using BankAccount.Core.Interfaces.Domain;
+
+namespace BankAccount.Core.Domain;
+
+public class Transaction(decimal amount, string description) : ITransaction
+{
+    public Guid TransactionId { get; } = Guid.NewGuid();
+    public decimal Amount { get; } = amount;
+    public DateTime TimeStamp { get; } = DateTime.UtcNow;
+    public string Description { get; } = description ?? throw new ArgumentNullException(nameof(description));
+    private bool disposed;
+
+    public void Dispose()
+    {
+        if (disposed) return;
+        GC.SuppressFinalize(this);
+        disposed = true;
+    }
+}
