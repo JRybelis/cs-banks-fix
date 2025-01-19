57d80167ae4262a33b70cd1269c31703c806dc0c
JRybelis <frizjr@protonmail.com>
2025-01-19 15:07:33 +0200

    Add Avalonia MVVM project, as MAUI unavailable

---
+++ BankAccountApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BankAccountApp.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ BankAccountApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BankAccountApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
