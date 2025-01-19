ff0099b4fb68d26c82c0f6bbd4a30248a9bd1072
JRybelis <frizjr@protonmail.com>
2025-01-19 20:43:16 +0200

    Add dependency injection for GUI

--- BankAccountApp/App.axaml.cs
+++ BankAccountApp/App.axaml.cs
@@ -1,11 +1,13 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using System.Linq;
 using Avalonia.Markup.Xaml;
 using BankAccountApp.ViewModels;
 using BankAccountApp.Views;
+using BankAccount.Core.Domain.Enums;
+using BankAccountBase.Core.Factories;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BankAccountApp;
 
@@ -20,12 +22,25 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
+            var services = new ServiceCollection();
+            
+            // Register services
+            services.AddSingleton(AccountFactory.CreateAccount(
+                AccountType.Checking, "Alice", 500m));
+            services.AddSingleton(AccountFactory.CreateAccount(
+                AccountType.Savings, "Bob", 5m));
+            
+            // Register ViewModels
+            services.AddSingleton<MainWindowViewModel>();
+            
+            var serviceProvider = services.BuildServiceProvider();
+            
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = serviceProvider.GetRequiredService<MainWindowViewModel>()
             };
         }
 
